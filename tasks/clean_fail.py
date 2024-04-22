aabb7b70cf617b17571bcb1de36d9a4b5e9f6038
atcaoyufei <atcaoyufei@gmail.com>
2024-04-22 14:52:09 +0800

    1.1

--- tasks/clean_fail.py
+++ tasks/clean_fail.py
@@ -8,7 +8,7 @@ class CleanFailTask(BaseTask):
         yield []
 
     async def after_start_worker(self):
-        d = get_bj_date(-3600 * 24 * 3)
+        d = get_bj_date(-3600 * 24 * 2)
         cond = {'status': 2, 'fail_count': {'$gte': 2}, 'last_time': {'$lt': d}}
         async for item in self.col.find(cond).limit(1000):
             await self.col.delete_one({'_id': item['_id']})
